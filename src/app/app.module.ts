6d4f90e6983602706c84ddaaf2a3153fcbd679ff
gsheshe <gsheshe13@gmail.com>
2023-04-09 13:08:51 +0200

    project without API accessing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { ElectronicsComponent } from './component/electronics/electronics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ElectronicsComponent
   ],
   imports: [
     BrowserModule,
